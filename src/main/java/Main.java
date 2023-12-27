a6cee900d2f55cda664a5ebf63d7281da3c489cb
SokolovNetology <sokolov180375@mail.ru>
2023-12-27 16:27:18 +0300

    gradle-csv-json

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,9 @@ import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.lang.reflect.Type;
@@ -19,16 +21,15 @@ public class Main {
 
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName1 = "data.csv";
-        List<Employee> list = parseCSV(columnMapping, fileName);
+        List<Employee> list = parseCSV(columnMapping, fileName1);
         String json = listToJson(list);
         String fileName2 = "data.xml";
-        List<Employee> list = parseXML("data.xml");
-
+        List<Employee> list = parseXML(fileName2);
     }
 
-    public static List<Employee> parseCSV(String[] columnMapping, String fileName1) throws IOException {
+    public static List<Employee> parseCSV(String [] columnMapping1, String fileName2) throws IOException {
 
-        try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
+        try (CSVReader reader = new CSVReader(new FileReader(fileName1))) {
             ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
             strategy.setType(Employee.class);
             strategy.setColumnMapping(columnMapping);
@@ -49,29 +50,34 @@ public class Main {
         }.getType();
         return gson.toJson(list, listType);
     }
-         public static void writeString(String json,String fileName){
-          try(FileWriter file = new FileWriter(fileName)){
-              file.write(json);
-              file.flush();
 
-          }catch (Exception e){
-              e.printStackTrace();
-          }
-          public static List<Employee> parseXML(String[] columnMapping,String fileName2){
-                 DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-                 DocumentBuilder builder = factory.newDocumentBuilder();
-                 Document doc = builder.parse(new File("data.xml"));
-                 Node root = doc.getDocumentElement();
-                 //System.out.println("Корневой узел: " + root.getNodeName());
-                 NodeList nodeList = root.getChildNodes()//список элементов
-                 for (int i=0;i< nodeList.getLenth;i++){
-                     Node node = nodeList.item(i);
-                     //System.out.println("Teкyщий элeмeнт: " + node.getNodeName());
-                 }
-                 if(Node.ELEMENT_NODE == node.getNodeType()){
-                    Element  employee = (Element) node;
-                 }
-                 return xml.parse;
-             }
-         }
+    public static void writeString(String json, String fileName) {
+        try (FileWriter file = new FileWriter(fileName)) {
+            file.write(json);
+            file.flush();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static List<Employee> parseXML(String fileName) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new File(fileName));
+        Node root = doc.getDocumentElement();
+        NodeList nodeList = root.getChildNodes();
+        for (int i = 0; i < nodeList.getLenth(); i++) {
+            Node node = nodeList.item(i);
+            Element employee = (Element) node;
+            Employee employee = new Employee();
+            employee.id = Long.parseLong(element.getElementsByTagName("id").item(0).getTextContent());
+            employee.firstName = element.getElementsByTagName("firstName").item(0).getTextContent();
+            employee.lastName = element.getElementsByTagName("lastName").item(0).getTextContent();
+            employee.country = element.getElementsByTagName("country").item(0).getTextContent();
+            employee.age = Integer.parseInt(element.getElementsByTagName("age").item(0).getTextContent());
+            list.add(employee);
+        }
+        return list;
+    }
 }
